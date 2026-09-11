43639cd6853dafb1d1966ed569104185936e27a8
htw0702tw <taiwan@htw0702.com>
2026-09-11 14:17:35 +0800

    Bootstrap CloudKit when MOOHSIA iOS launches

--- MOOHSIA-Apple/iOS/MOOHSIAApp.swift
+++ MOOHSIA-Apple/iOS/MOOHSIAApp.swift
@@ -2,7 +2,13 @@ import SwiftUI
 
 @main
 struct MOOHSIAApp: App {
+    @StateObject private var cloudKit = CloudKitSyncService()
+
     var body: some Scene {
-        WindowGroup { ContentView() }
+        WindowGroup {
+            ContentView()
+                .environmentObject(cloudKit)
+                .task { await cloudKit.bootstrap() }
+        }
     }
 }
